6a2208c0e671ede3d91518f3056e9e024fc46b78
qianxi0410 <894871277@qq.com>
2022-04-07 19:28:03 +0800

    feat: linkedlist basic

---
+++ lists/linkedlist/linkedlist.go
@@ -0,0 +1,288 @@
+package linkedlist
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/qianxi0410/gogds/lists"
+	"github.com/qianxi0410/gogds/utils"
+)
+
+func assertListImplementation() {
+	var _ lists.Lists[int] = New[int]()
+	var _ lists.Lists[string] = New[string]()
+	var _ lists.Lists[float32] = New[float32]()
+	var _ lists.Lists[complex128] = New[complex128]()
+	var _ lists.Lists[time.Time] = New[time.Time]()
+	var _ lists.Lists[bool] = New[bool]()
+	var _ lists.Lists[struct{}] = New[struct{}]()
+}
+
+// node represents a node in a linked list
+// each node has a value and a pointer to the next node.
+type node[T any] struct {
+	val  T
+	next *node[T]
+}
+
+// List represents a linked list
+// the first node is the head of the list and the last node is the tail of the list.
+type List[T comparable] struct {
+	first *node[T]
+	last  *node[T]
+	size  int
+}
+
+// New returns a new list.
+func New[T comparable](values ...T) *List[T] {
+	list := &List[T]{}
+
+	if len(values) > 0 {
+		list.Add(values...)
+	}
+
+	return list
+}
+
+// Add adds a value to the list.
+func (l *List[T]) Add(values ...T) {
+	for _, v := range values {
+		n := &node[T]{val: v}
+		if l.size == 0 {
+			l.first = n
+			l.last = n
+		} else {
+			l.last.next = n
+			l.last = n
+		}
+		l.size++
+	}
+}
+
+// Append adds a value to the end of the list.
+func (l *List[T]) Append(values ...T) {
+	l.Add(values...)
+}
+
+// Prepend adds a value to the beginning of the list.
+func (l *List[T]) Prepend(values ...T) {
+	for i := len(values) - 1; i >= 0; i-- {
+		n := &node[T]{val: values[i], next: l.first}
+
+		l.first = n
+		if l.size == 0 {
+			l.last = n
+		}
+
+		l.size++
+	}
+}
+
+// Get returns the value at the given index.
+func (l *List[T]) Get(index int) (t T, _ bool) {
+
+	if !l.checkIdx(index) {
+		return t, false
+	}
+
+	cur := l.first
+	for i := 0; i < index; i++ {
+		cur = cur.next
+	}
+	return cur.val, true
+}
+
+// Remove removes the value at the given index.
+func (l *List[T]) Remove(index int) (t T, _ bool) {
+	if !l.checkIdx(index) {
+		return t, false
+	}
+
+	if index == 0 {
+		t = l.first.val
+		l.first = l.first.next
+		l.size--
+		return t, true
+	}
+
+	if l.size == 1 {
+		l.Clear()
+		return
+	}
+
+	cur := l.first
+	for i := 0; i < index-1; i++ {
+		cur = cur.next
+	}
+
+	t = cur.next.val
+	cur.next = cur.next.next
+	l.size--
+
+	return t, true
+}
+
+// Contains checks if the given value is in the list.
+func (l *List[T]) Contains(values ...T) bool {
+	if len(values) == 0 {
+		return true
+	}
+
+	if l.size == 0 {
+		return false
+	}
+
+	for _, v := range values {
+		found := false
+		for ele := l.first; ele != nil; ele = ele.next {
+			if ele.val == v {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+// Values return a copy of the list's elements.
+func (l *List[T]) Values() []T {
+	values := make([]T, l.size)
+
+	for e, ele := 0, l.first; ele != nil; e, ele = e+1, ele.next {
+		values[e] = ele.val
+	}
+	return values
+}
+
+// IndexOf return the index of the first element matching the given value.
+func (l *List[T]) IndexOf(value T) int {
+	if l.size == 0 {
+		return -1
+	}
+
+	for idx, ele := 0, l.first; ele != nil; idx, ele = idx+1, ele.next {
+		if ele.val == value {
+			return idx
+		}
+	}
+
+	return -1
+}
+
+// Empty returns true if the list is empty.
+func (l *List[T]) Empty() bool {
+	return l.size == 0
+}
+
+// Size returns the number of elements in the container.
+func (l *List[T]) Size() int {
+	return l.size
+}
+
+// Sort sort values in this container.
+func (l *List[T]) Sort(c utils.Comparator[T]) {
+	// FIXME: this way is too slow, maybe use some other sort algorithm.
+	if l.size < 2 {
+		return
+	}
+
+	values := l.Values()
+	utils.Sort(values, c)
+
+	l.Clear()
+	l.Add(values...)
+}
+
+// Swap swaps values in this container.
+func (l *List[T]) Swap(i, j int) {
+	if l.checkIdx(i) && l.checkIdx(j) && i != j {
+		var ele1, ele2 *node[T]
+
+		for e, cur := 0, l.first; cur != nil; e, cur = e+1, cur.next {
+			if i == e {
+				ele1 = cur
+			} else if j == e {
+				ele2 = cur
+			}
+		}
+
+		ele1.val, ele2.val = ele2.val, ele1.val
+	}
+}
+
+// Insert inserts values in this container.
+// The values are inserted before the given index.
+func (l *List[T]) Insert(index int, values ...T) {
+	if !l.checkIdx(index) {
+		if index == l.size {
+			l.Append(values...)
+		}
+		return
+	}
+
+	l.size += len(values)
+
+	dummy := &node[T]{next: l.first}
+	cur := dummy
+
+	for i := 0; i < index; i++ {
+		cur = cur.next
+	}
+
+	next := cur.next
+	for _, v := range values {
+		node := &node[T]{val: v, next: cur.next}
+		cur.next = node
+		cur = node
+	}
+
+	cur.next = next
+
+	l.first = dummy.next
+}
+
+// Set sets the value at the given index.
+// If the index is equal to the sie, the value is appended to the list.
+func (l *List[T]) Set(index int, value T) {
+	if !l.checkIdx(index) {
+		if index == l.size {
+			l.Add(value)
+		}
+		return
+	}
+
+	cur := l.first
+	for index > 0 {
+		cur = cur.next
+		index--
+	}
+
+	cur.val = value
+}
+
+// String returns a string representation of the list.
+func (l *List[T]) String() string {
+	str := "SinglyLinkedList\n"
+	values := []string{}
+	for element := l.first; element != nil; element = element.next {
+		values = append(values, fmt.Sprintf("%v", element.val))
+	}
+	str += strings.Join(values, ", ")
+	return str
+}
+
+// Clear removes all elements in the container.
+func (l *List[T]) Clear() {
+	l.size = 0
+	l.first = nil
+	l.last = nil
+}
+
+// checkIdx checks if the given index is valid.
+func (l *List[T]) checkIdx(index int) bool {
+	return index >= 0 && index < l.size
+}
---
+++ lists/linkedlist/linkedlist_test.go
@@ -0,0 +1,156 @@
+package linkedlist
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	l := New[int]()
+
+	assert.Equal(t, l.first, l.last)
+	assert.Nil(t, l.first)
+	assert.Nil(t, l.last)
+
+	l.Add(1)
+	assert.Equal(t, 1, l.first.val)
+	assert.Equal(t, 1, l.last.val)
+	assert.Equal(t, l.last, l.first)
+
+	l.Add(2)
+	assert.Equal(t, 2, l.last.val)
+}
+
+func TestPrepend(t *testing.T) {
+	l := New(1)
+
+	l.Prepend(2)
+	assert.Equal(t, 2, l.first.val)
+
+	l.Prepend(3, 4)
+	assert.Equal(t, 3, l.first.val)
+}
+
+func TestGet(t *testing.T) {
+	l := New(1, 2, 3)
+
+	v, ok := l.Get(0)
+	assert.True(t, ok)
+	assert.Equal(t, 1, v)
+
+	v, ok = l.Get(1)
+	assert.True(t, ok)
+	assert.Equal(t, 2, v)
+
+	v, ok = l.Get(2)
+	assert.True(t, ok)
+	assert.Equal(t, 3, v)
+
+	v, ok = l.Get(10)
+	assert.False(t, ok)
+	assert.Equal(t, 0, v)
+}
+
+func TestRemove(t *testing.T) {
+	l := New(1, 2, 3, 4, 5)
+
+	v, ok := l.Remove(0)
+	assert.True(t, ok)
+	assert.Equal(t, 1, v)
+
+	v, ok = l.Remove(2)
+	assert.True(t, ok)
+	assert.Equal(t, 4, v)
+}
+
+func TestContains(t *testing.T) {
+	l := New(1, 2, 3)
+
+	ok := l.Contains(1)
+	assert.True(t, ok)
+
+	ok = l.Contains(1, 3)
+	assert.True(t, ok)
+
+	ok = l.Contains()
+	assert.True(t, ok)
+
+	ok = l.Contains(-1)
+	assert.False(t, ok)
+
+}
+
+func TestIndexOf(t *testing.T) {
+	l := New("1", "2", "3")
+
+	idx := l.IndexOf("1")
+	assert.Equal(t, 0, idx)
+
+	idx = l.IndexOf("3")
+	assert.Equal(t, 2, idx)
+
+	idx = l.IndexOf("-1")
+
+	assert.Equal(t, -1, idx)
+}
+
+func TestSort(t *testing.T) {
+	l := New(-1, 10, 1, 2, 3)
+
+	l.Sort(func(a, b int) bool {
+		return a < b
+	})
+
+	assert.Equal(t, l.Values()[0], -1)
+	assert.Equal(t, l.Values()[l.Size()-1], 10)
+}
+
+func TestSwap(t *testing.T) {
+	l := New(1, 2, 3, 4)
+
+	l.Swap(0, 3)
+
+	assert.Equal(t, l.Values()[0], 4)
+	assert.Equal(t, l.Values()[3], 1)
+}
+
+func TestInsert(t *testing.T) {
+	l := New[int]()
+
+	l.Insert(0, 1, 2, 3, 4)
+
+	// 1 2 3 4
+	assert.Equal(t, l.Values()[0], 1)
+	assert.Equal(t, l.Values()[1], 2)
+	assert.Equal(t, l.Values()[2], 3)
+	assert.Equal(t, l.Values()[3], 4)
+
+	l.Insert(3, 5, 6)
+	// 1 2 3 5 6 4
+	assert.Equal(t, l.Values()[3], 5)
+	assert.Equal(t, l.Values()[4], 6)
+	assert.Equal(t, l.Values()[5], 4)
+
+	l.Insert(1, 7, 8)
+	// 1 7 8 2 3 5 6 4
+	assert.Equal(t, l.Values()[1], 7)
+	assert.Equal(t, l.Values()[2], 8)
+	assert.Equal(t, l.Values()[3], 2)
+}
+
+func TestSet(t *testing.T) {
+	l := New(1, 2, 3, 4)
+
+	l.Set(0, 5)
+	assert.Equal(t, l.Values()[0], 5)
+
+	l.Set(1, 6)
+	assert.Equal(t, l.Values()[1], 6)
+
+	l.Set(2, 7)
+	assert.Equal(t, l.Values()[2], 7)
+
+	l.Set(3, 8)
+	assert.Equal(t, l.Values()[3], 8)
+}
