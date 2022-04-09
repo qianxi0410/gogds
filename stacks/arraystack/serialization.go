53a9c905c7928dc392d5cd90bd4e8e5fb081648f
qianxi0410 <894871277@qq.com>
2022-04-09 09:54:06 +0800

    feat: arraystack basic

---
+++ stacks/arraystack/serialization.go
@@ -0,0 +1,23 @@
+package arraystack
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"github.com/qianxi0410/gogds/containers"
+)
+
+func assertSerializationImplementation[T comparable]() {
+	var _ containers.JSONSerializer = New[T]()
+	var _ containers.JSONDeserializer = New[T]()
+}
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+// ToJSON outputs the JSON representation of list's elements.
+func (s *Stack[T]) ToJSON() ([]byte, error) {
+	return s.list.ToJSON()
+}
+
+// FromJSON populates list's elements from the input JSON representation.
+func (s *Stack[T]) FromJSON(data []byte) error {
+	return s.list.FromJSON(data)
+}
