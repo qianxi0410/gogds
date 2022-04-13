7ba8baac36933cc649bd586b6b90673876028015
qianxi0410 <894871277@qq.com>
2022-04-13 14:30:13 +0800

    feat: add interface contraint

--- containers/enumerable.go
+++ containers/enumerable.go
@@ -26,3 +26,22 @@ type EnumerableWithIndex[T any] interface {
 	// if no element matches the criteria.
 	Find(func(index int, value T) bool) (int, T)
 }
+
+// EnumerableWithKey provides functions for ordered containers whose values whose elements are key/value pairs.
+type EnumerableWithKey[K comparable, V any] interface {
+	// Each calls the given function once for each element, passing that element's key and value.
+	Each(func(key K, value V))
+
+	// Any passes each element of the container to the given function and
+	// returns true if the function ever returns true for any element.
+	Any(func(key K, value V) bool) bool
+
+	// All passes each element of the container to the given function and
+	// returns true if the function returns true for all elements.
+	All(func(key K, value V) bool) bool
+
+	// Find passes each element of the container to the given function and returns
+	// the first (key,value) for which the function is true or nil,nil otherwise if no element
+	// matches the criteria.
+	Find(func(key K, value V) bool) (K, V)
+}
