20f30261fa72f70d4b52e9b68ba0a75b656dce25
qianxi0410 <894871277@qq.com>
2022-04-15 10:35:58 +0800

    chore: chnage return value

--- queues/queues.go
+++ queues/queues.go
@@ -5,8 +5,8 @@ import "github.com/qianxi0410/gogds/containers"
 // Queue interface that all queues implement
 type Queues[V any] interface {
 	Enqueue(v V)
-	Dequeue() V
-	Peek() V
+	Dequeue() (V, bool)
+	Peek() (V, bool)
 
 	containers.Container[V]
 }
