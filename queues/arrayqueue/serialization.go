1fec09f35e87c4257e5fa2b333b47f4faddbbbed
qianxi0410 <894871277@qq.com>
2022-04-15 10:36:05 +0800

    faet: arrayqueue

---
+++ queues/arrayqueue/serialization.go
@@ -0,0 +1,21 @@
+package arrayqueue
+
+import (
+	"github.com/qianxi0410/gogds/containers"
+)
+
+// nolint
+func assertSerializationImplementation[T comparable]() {
+	var _ containers.JSONSerializer = New[T]()
+	var _ containers.JSONDeserializer = New[T]()
+}
+
+// ToJSON outputs the JSON representation of list's elements.
+func (q *Queue[T]) ToJSON() ([]byte, error) {
+	return q.list.ToJSON()
+}
+
+// FromJSON populates list's elements from the input JSON representation.
+func (q *Queue[T]) FromJSON(data []byte) error {
+	return q.list.FromJSON(data)
+}
